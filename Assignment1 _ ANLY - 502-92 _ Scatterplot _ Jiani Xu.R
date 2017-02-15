2212b0ea55114797c78fd3292f54d1cab3c2bcf7
Nicolestudying <JXu@my.harrisburgu.edu>
2017-02-14 21:38:30 -0500

    Update Assignment1 _ ANLY - 502-92 _ Scatterplot _ Jiani Xu.R

--- Assignment1 _ ANLY - 502-92 _ Scatterplot _ Jiani Xu.R
+++ Assignment1 _ ANLY - 502-92 _ Scatterplot _ Jiani Xu.R
@@ -3,9 +3,9 @@ getwd()
 
 NewTable = read.csv("/Users/jiani0217/desktop/HU/Workbook_Example_Bivariate_dataset.csv")
 
-plot(NewTable$Profit, NewTable$Number.of.Defective.Items, 
+plot(NewTable$Profit, NewTable$Number.of.Defective.Items,
      main = "Scatterplot_ New Table", xlab = "Profit Numbers", ylab = "Defective Number", 
-     frame.plot = FALSE 
-     col = "orange"
-     pch = 15
-     cex=1.2
+     frame.plot = FALSE, 
+     col = "orange",
+     pch = 15,
+     cex=1.2)
