b13cdb711974fd4fadcbc7c6f1569dd77078866c
Nicolestudying <JXu@my.harrisburgu.edu>
2017-02-14 21:34:47 -0500

    Add files via upload

---
+++ Assignment1 _ ANLY - 502-92 _ Scatterplot _ Jiani Xu.R
@@ -0,0 +1,11 @@
+setwd ("/Users/jiani0217/Desktop/HU")
+getwd()
+
+NewTable = read.csv("/Users/jiani0217/desktop/HU/Workbook_Example_Bivariate_dataset.csv")
+
+plot(NewTable$Profit, NewTable$Number.of.Defective.Items, 
+     main = "Scatterplot_ New Table", xlab = "Profit Numbers", ylab = "Defective Number", 
+     frame.plot = FALSE 
+     col = "orange"
+     pch = 15
+     cex=1.2
