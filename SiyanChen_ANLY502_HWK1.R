a22f5b4947c1e2f740826e3e91bb07affa29e388
DaisyDaisyChen <siyanchen.syc@gmail.com>
2017-02-15 21:20:13 -0500

    SiyanChen_ANLY502_HWK1
    Almmotamedi/Assignment1-ANLY-502-92

---
+++ SiyanChen_ANLY502_HWK1.R
@@ -0,0 +1,17 @@
+setwd ("/Users/siyanchen/Desktop/Harrisburg/ANLY 502/HWK 1")
+getwd()
+
+scatter = read.csv("/Users/siyanchen/Desktop/Harrisburg/ANLY 502/HWK 1/Workbook_Example_Bivariate_dataset.csv")
+summary(scatter)
+
+plot(scatter$Number.of.Defective.Items, scatter$Profit,
+     type="p",
+     ylab="Profit",
+     xlab="Number of Defective Items",
+     main="Profit & Number of Defective Items",
+     frame.plot=TRUE, 
+     pch=20,  #plotting character
+     cex=0.6, #character expansion
+     col="green")
+
+scatter$Profit
